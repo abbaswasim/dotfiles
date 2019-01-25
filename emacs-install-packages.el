58b81b42e25806c074fad9ad45c8fe008cddcaf1
Wasim Abbas <abbas.wasim@gmail.com>
2019-01-25 19:15:19 +0000

    Updates package list to install and adds pip install jedi

--- emacs-install-packages.el
+++ emacs-install-packages.el
@@ -4,13 +4,12 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-(setq install-list '(neotree ace-jump-mode ag cmake-mode color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow company-c-headers company-cmake company-flx company-rtags company-web diminish evil-easymotion avy evil-indent-textobject evil-leader evil-mc evil-nerd-commenter evil-numbers evil-org evil-search-highlight-persist evil-surround evil-tabs elscreen evil-tutor flx flycheck-clangcheck flycheck-pos-tip flycheck-rtags helm-ag helm-company company helm-flycheck flycheck helm-helm-commands helm-projectile helm-rtags helm helm-core async highlight iedit let-alist linum-relative litable dash package-utils popup pos-tip powerline-evil powerline evil goto-chg projectile pkg-info epl restart-emacs rtags s seq srefactor stickyfunc-enhance undo-tree web-completion-data yasnippet zenburn-theme clang-format string-inflection))
+(setq install-list '(elpy neotree yasnippet-snippets clang-format string-inflection web-completion-data undo-tree seq s restart-emacs epl pkg-info projectile goto-chg pos-tip dash let-alist highlight async helm-core flx avy litable company semantic cc-mode json saveplace package linum-off powerline linum linum-relative helm-rtags flycheck-rtags company-rtags package-utils srefactor helm flycheck evil zenburn-theme yasnippet stickyfunc-enhance powerline-evil popup iedit helm-projectile helm-helm-commands helm-gtags helm-flycheck helm-company helm-ag flycheck-pos-tip evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-leader evil-indent-textobject evil-easymotion diminish company-web company-flx company-cmake company-c-headers color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cmake-mode ag ace-jump-mode))
 
 (package-refresh-contents)
 
--- setup-dot-files.sh
+++ setup-dot-files.sh
@@ -8,33 +8,45 @@ rm .fzf.zsh
 rm -rf .vim
 rm .vimrc
 rm .zshrc
+echo "Old symbolic links removed"
 
 echo "Creating new symbolic links..."
 ln -s dotfiles/.emacs.d .emacs.d
 ln -s dotfiles/.vim .vim
 ln -s dotfiles/.vimrc .vimrc
 ln -s dotfiles/.zshrc .zshrc
+echo "Symbolic links created"
 
 echo "Cloning Vundle for vim..."
 git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Vundle installed"
 
 echo "Installing vim packages..."
 vim +PluginInstall +qall
+echo "All vim plugins installed"
 
 popd
 
 echo "Cloning oh-my-zsh..."
 git clone --depth 1 https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+echo "oh-my-zsh installed"
 
 echo "Cloning and installing fzf..."
 git clone --depth 1 https://github.com/junegunn/fzf.git .fzf
 printf 'y\ny\ny\n' | ./.fzf/install
 pushd .oh-my-zsh/themes/
+echo "fzf installed"
 
 echo "Cloning and installing oh-my-zsh theme powerlevel9k..."
 git clone --depth 1 https://github.com/bhilburn/powerlevel9k.git
+echo "powerlevel installed"
 
 popd
 
 echo "Running emacs packages install script..."
 emacs --script ./emacs-install-packages.el
+echo "All emacs packages installed"
+
+echo "Installing jedi for python mode"
+pip install jedi
+echo "Jedi installed"
