19318917a8d6ebcdef3aac862319ba1add9be3d7
Wasim Abbas <abbas.wasim@gmail.com>
2019-04-25 17:39:34 +0100

    Update orgmode bullets size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -201,8 +201,8 @@
  ;; If there is more than one, they won't work right.
  '(evil-search-highlight-persist-highlight-face ((t (:background "#3e999f" :foreground "#ffffff"))))
  '(linum-relative-current-face ((t (:inherit linum :weight bold :underline "#555"))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
